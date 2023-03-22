5848be317e53799d9a6ee8343d1247aa84cf5e1a
Deblox <deblox.160999@gmail.com>
2023-03-22 09:14:17 +0100

    fix boostrap 5.3 -> boostrap 5.1.3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
 </head>
 <body>
 
@@ -18,31 +18,37 @@
    
     <section>
 
-    <div class="container">
+    <div class="container mx-auto">
 
-    <form action="/src/php/trie.php" method="post">
-            <select name="trie"  >
-                <option value="triebase" selected hidden>Tri par</option>
-                <option value="artist_az">Artiste A - Z</option>
-                <option value="artist_za">Artiste Z - A</option>
-                <option value="album_az">Album A - Z</option>
-                <option value="album_za">Album Z - A</option>
-                <option value="genre_trie">Genre</option>
-                <option value="label_trie">Label</option>
-                <option value="prix_asc">Prix croissant</option>
-                <option value="prix_desc">Prix décroissant</option>
-            </select>
-            <input type="submit" class="btn btn-secondary btn-sm"/>
-    </form>
+
+<form action="/src/php/trie.php" method="post">
+<div class="input-group justify-content-end">
+    <select name="trie" class="btn btn-secondary">
+        <option value="triebase" selected hidden>Tri par</option>
+        <option value="artist_az">Artiste A - Z</option>
+        <option value="artist_za">Artiste Z - A</option>
+        <option value="album_az">Album A - Z</option>
+        <option value="album_za">Album Z - A</option>
+        <option value="genre_trie">Genre</option>
+        <option value="label_trie">Label</option>
+        <option value="prix_asc">Prix croissant</option>
+        <option value="prix_desc">Prix décroissant</option>
+    </select>
+    <input type="submit" class="btn btn-secondary btn-sm rounded"/>
+    </div>
+</form>
+
+
+
                 <!--------- affichage disque ----------->
                 <?php include("./src/php/selectdisc.php");
                 ?>                
-    </div>
+</div>
     </section>
 
     <!-- Script JS -->
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
